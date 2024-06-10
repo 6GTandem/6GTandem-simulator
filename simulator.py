7743148c49f565acbbc161b7ace8e3a01ecc1b5f
Gilles Callebaut, PhD <callebaut.gilles@gmail.com>
2024-06-10 21:42:05 +0200

    Create simulator.py

---
+++ simulator.py
@@ -0,0 +1,6 @@
+# if any troubles with the engine, please consult https://www.mathworks.com/help/matlab/matlab_external/install-the-matlab-engine-for-python.html and our readme
+import matlab.engine
+eng = matlab.engine.start_matlab()
+
+if __name__ == "__main__":
+  pass
