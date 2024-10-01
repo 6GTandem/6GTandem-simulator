b1a61470a2cb3ef83b13f462e5ffdd6bd489212a
Tijl Schepens <tijl.schepens@kuleuven.be>
2024-10-01 16:43:37 +0200

    Implemented all models in Python. Nothing has been tested yet.

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,11 @@
 import math
 import models.utils
 import numpy as np
+import numpy.fft
 import numpy.random
 
 from abc import ABC, abstractmethod
+from scipy.signal import lfilter
 
 
 def db_to_power(db_value: float | int):
@@ -12,9 +14,16 @@ def db_to_power(db_value: float | int):
     The power/decibel relationship is as follows:
     db_value = 10 * log10(power_value)
     """
-    power = 10.0 ** (db_value / 10.0)
+    return 10.0 ** (db_value / 10.0)
 
-    return power
+
+def power_to_db(power_value: float | int):
+    """Convert a power value to decibels (dB).
+
+    The power/decibel relationship is as follows:
+    db_value = 10 * log10(power_value)
+    """
+    return 10 * np.log10(power_value)
 
 
 def db_to_magnitude(db_value: float | int):
@@ -23,9 +32,7 @@ def db_to_magnitude(db_value: float | int):
     The power/decibel relationship is as follows:
     db_value = 20 * log10(power_value)
     """
-    power = 10.0 ** (db_value / 20.0)
-
-    return power
+    return 10.0 ** (db_value / 20.0)
 
 
 class Component(ABC):
@@ -53,6 +60,16 @@ class Component(ABC):
     @abstractmethod
     def run(self, x):
         ...
+
+
+class Coupler(Component):
+    def __init__(self, damping: float = 0):
+        self.damping = damping
+
+        super().__init__()
+
+    def run(self, x):
+        return x * db_to_magnitude(self.damping)
 
 
 class Amplifier(Component):
@@ -83,24 +100,10 @@ class Amplifier(Component):
         """
         self.gain = gain
         self._max_output_amplitude = max_out_amp
-        self._noise_var = noise_var
+        self.noise_var = noise_var
         self.smoothness = smoothness
 
         super().__init__()
-
-    @property
-    def noise_var(self):
-        return self._noise_var
-
-    @noise_var.setter
-    def noise_var(self, t, b, nfdb):
-        k = 1.3806E-23
-        noise_density = k * t * db_to_power(nfdb)
-        noise_power = noise_density * b
-        # P = U^2/R
-        voltage_power = noise_power * 50
-        # Noise variance per channel.
-        self._noise_var = voltage_power / 2
 
     @property
     def max_output_amplitude(self):
@@ -122,8 +125,17 @@ class Amplifier(Component):
         """
         self.gain = db_to_magnitude(pout - pin)
 
+    def set_noise_var(self, t, b, nfdb):
+        k = 1.3806e-23
+        noise_density = k * t * db_to_power(nfdb)
+        noise_power = noise_density * b
+        # P = U^2/R
+        voltage_power = noise_power * 50
+        # Noise variance per channel.
+        self.noise_var = voltage_power / 2
+
     def run(self, x):
-        x = self.gain / self._max_output_amplitude * (x + math.sqrt(self._noise_var) * (
+        x = self.gain / self._max_output_amplitude * (x + math.sqrt(self.noise_var) * (
             numpy.random.normal(size=np.shape(x)) + 1j * numpy.random.normal(size=np.shape(x))))
 
         match self.mode:
@@ -171,3 +183,333 @@ class Amplifier(Component):
                 xout = models.utils.softlimiter(x, self.smoothness)
 
         return self._max_output_amplitude * xout
+
+
+class Dac(Component):
+    def __init__(self, trunc_level: float = np.inf, nobits: float = np.inf):
+        self.trunc_level = trunc_level
+        self.nobits = nobits
+
+        super().__init__()
+
+    def run(self, yin):
+        if self.trunc_level > 1e98:
+            return yin
+        if self.nobits > 20:
+            yout = models.utils.limiter(yin, self.trunc_level)
+        else:
+            step = 2 * self.trunc_level / (2 ** self.nonbits)
+            yout = models.utils.limiter(
+                round(yin / step) * step, self.trunc_level)
+
+        return yout
+
+
+class Fiber(Component):
+    def __init__(self, length: float = 5, damping_per_meter: float = 5, fs: float = 15e9,
+                 filter: float = 1):
+        """Initialize a fiber component.
+
+        :param delay: Delay in number of samples.
+        :param damping: The damping in dB.
+        """
+        self.length = length
+        self.damping_per_meter = damping_per_meter
+        self.fs = fs
+        self.filter = filter
+
+        super().__init__()
+
+    def run(self, x):
+        xout = models.utils.delay(lfilter(self.filter, 1, x), self.delay)
+
+        return models.utils.setdbm(xout, self.utils.getdbm(x) - self.damping)
+
+    @property
+    def delay(self):
+        return (self.length * 1.5) / (3e8 * self.fs)
+
+    @property
+    def damping(self):
+        return self.length * self.damping_per_meter
+
+
+class IQModem(Component):
+    def __init__(self, iqi_coef: int = 0, iqi_filter: int = 1, iqi_delay_imbalance: int = 0, dc_offset: int = 0):
+        self.iqi_coef = iqi_coef
+        self.iqi_filter = iqi_filter
+        self.iqi_delay_imbalance = iqi_delay_imbalance
+        self.dc_offset = dc_offset
+
+        super().__init__()
+
+    def run(self, yin, phasor):
+        assert (len(yin) == len(phasor)
+                ), "Yin and Phasor data lengths do not match."
+
+        match self.mode:
+            case 'ideal':
+                yout = yin
+            case 'filter':
+                d = np.shape((self.iqi_filter - 1) / 2)
+                xc = lfilter(self.iqi_filter, 1, [
+                             [np.conj(yin)], [np.zeros(np.shape(d))]])
+                xc = xc[d + 1:]
+                yout = yin + xc + self.dc_offset
+            case 'static':
+                yout = models.utils.delay(np.real(yin), -self.iqi_delay_imbalance / 2) + \
+                    1j * models.utils.delay(np.imag(yin),
+                                            self.iqi_delay_imbalance / 2)
+                yout = yout + self.iqi_coef * np.conj(yout) + self.dc_offset
+
+        return yout * phasor
+
+
+class Link(Component):
+    def __init__(self, amp: Amplifier, fiber: Fiber, coupler_in: Coupler, coupler_out: Coupler):
+        self.amp = amp
+        self.fiber = fiber
+        self.coupler_in = coupler_in
+        self.coupler_out = coupler_out
+
+        super().__init__()
+
+    def run(self, y):
+        z = self.fiber.run(y)
+        x1 = self.coupler_in.run(z)
+        x = self.amp.run(x1)
+
+        return self.coupler_out.run(x)
+
+
+class Oscillator(Component):
+    """Oscillator class.
+
+    Example usage:
+        >>> x = Oscillator()
+        >>> pn = x.run_phase(1000) # Generates 1000 phase noise samples.
+
+    # modes of pn generation 'ideal', 'cfo', 'model', 'spectrum'
+    """
+
+    def __init__(self, fs: float = 1.5e9, cfo: float = 0, cfo_std: float = 0, l100_db: float = -200,
+                 linf_db: float = -300, f3db: float = 0, freq: list = [], spec: list = []):
+        """
+
+        :param fs: Sampling frequency in Hertz.
+        :param cfo: Carrier frequency offset (receiver only) in Hertz.
+        :param cfo_std: Carrier frequency random offset (receiver only) in Hertz
+        :param l100_db: Pn level at 100 kHz in dB.
+        :param linf_db: With pn level in dB.
+        :param f3db: Cut-off frequency in Hertz.
+        :param freq: Definition of spectrum response in Hertz.
+        :param spec: Definition of spectrum response in dB.
+        """
+        self.fs = fs
+        self.cfo = cfo
+        self.cfo_std = cfo_std
+        self.l100_db = l100_db
+        self.linf_db = linf_db
+        self.f3db = f3db
+        self.freq = freq
+        self.spec = spec
+
+        self.last_phasor = 0
+        self.current_phase = numpy.random.normal() * 2 * np.pi
+
+        super().__init__()
+
+    @property
+    def l0_db(self):
+        return power_to_db(1e10 * (db_to_power(self.l100_db) - db_to_power(self.linf_db)) / self.f3db ** 2)
+
+    @property
+    def a(self):
+        return np.exp(-2 * np.pi * self.f3db / self.fs)
+
+    @property
+    def pnvar(self):
+        a2 = self.a ** 2
+        if (1 - a2) < 1e-10:
+            pnvar = 4 * np.pi ** (2 * 1e10 *
+                                  db_to_power(self.l100_db) / self.fs)
+        else:
+            pnvar = (1 - a2) * np.pi * 1e10 * \
+                db_to_power(self.l100_db) / self.f3db
+
+        return pnvar
+
+    @property
+    def pnmean(self):
+        # Extra variance to the CFO offset.
+        return 2 * np.pi * (self.cfo + self.cfo_std * np.random.normal()) / self.fs
+
+    @property
+    def whitepnvar(self):
+        return db_to_power(self.linf_db) * self.fs
+
+    def run_phase(self, nosamples):
+        match self.mode:
+            case 'ideal':
+                fi = np.zeros(nosamples)
+            case 'cfo':
+                fi = self.run_phase_cfo(nosamples)
+            case 'model':
+                fi = self.run_phase_model(nosamples)
+            case 'spectrum':
+                fi = self.run_phase_spectrum(nosamples)
+            case 'iid':
+                fi = self.run_phase_iid(nosamples)
+
+        return fi
+
+    def run(self, nosamples):
+        x = np.exp(1j * self.run_phase(nosamples))
+        # Add AWGN
+        if self.mode == 'model':
+            x = x + np.sqrt(self.whitepnvar) * (numpy.random.normal(
+                size=np.shape(x)) + 1j * numpy.random.normal(size=np.shape(x)))
+
+        self.last_phasor = x
+
+        return x
+
+    def variance_phase_spectrum(self):
+        n = 2 ** 23
+        f = [[np.finfo(float).eps], [self.freq], [self.fs]]
+        s = [[self.spec[0]], [self.spec], [self.spec[-1]]]
+        flin = np.transpose(np.linspace(0, self.fs / 2, n + 1))
+        x = db_to_power(np.interp(np.log(f), s, np.log(flin)) / 2)
+        x = x * np.sqrt(flin[1] - flin[0]) * n * 2
+
+        return sum(abs(x[1:]) ** 2) / 2 / len(x) ** 2
+
+    def run_phase_spectrum(self, nosamples):
+        n = nosamples / 2
+        f = ([np.finfo(float).eps], [np.transpose([self.freq])], [self.fs])
+        s = [[self.spec[0]], [np.transpose([self.spec])], [self.spec[-1]]]
+        flin = np.linspace(0, self.fs / 2, n + 1)
+        x = db_to_power(np.interp(np.log(f), s, np.log(flin)) / 2)
+        x = x * np.sqrt(flin[1] - flin[0]) * n * 2
+        x = x * np.sqrt(0.5) * (models.utils.randn_c(np.shape(x)))
+
+        fi = np.real(numpy.fft.ifft(
+            [[0], [x[1:]], [0], [np.flipud(np.conj(x[1:]))]]))
+        fi = fi + self.run_phase_cfo(np.shape(fi))
+        self.current_phase = fi[-1]
+
+        return fi
+
+    def run_phase_model(self, nosamples):
+        u = np.sqrt(self.pnvar) * numpy.random.normal(nosamples) + self.pnmean
+        fi = lfilter(1, [1 - self.a], [[self.current_phase], [u]])
+        fi = fi[1:]
+        self.current_phase = fi[-1]
+
+        return fi
+
+    def run_phase_cfo(self, nosamples):
+        fi = np.cumsum(self.pnmean * np.ones(nosamples)) + self.current_phase
+        self.current_phase = fi[-1]
+
+        return fi
+
+
+class Transmitter(Component):
+    """Definition of a transmitter
+
+    Components: c_oscillator, c_iqmod, c_pa, c_dac, c_antenna
+    Usage:
+        >>> tx = Transmitter()
+        >>> y = tx.run(x)
+    """
+
+    def __init__(self, oscillator: Oscillator, iqmod: IQModem, amp: Amplifier, dac: Dac, delay: float = 0):
+        self.oscillator = oscillator
+        self.iqmodem = iqmod
+        self.amplifier = amp
+        self.dac = dac
+        self.delay = delay
+
+        super().__init__()
+
+    def run(self, x, phasor=None):
+        if phasor is None:
+            phasor = self.oscillator.run(len(x))
+
+        x1 = self.dac.run(x)
+        x2 = self.iqmodem.run(x1, phasor)
+        xout = self.amplifier.run(x2)
+
+        if self.delay != 0:
+            xout = models.utils.delay(xout, self.delay)
+
+        return xout
+
+
+class RadioStripe(Component):
+    """Class representing a radio stripe.
+
+    This class implements a radiostripe in the 6GTANDEM project.
+    The stripe consists of a transmitter component, and a set of links.
+    Usage:
+        >>> rs = RadioStripe()
+        >>> y = rs.run(x) # Y is a matrix
+    """
+
+    def __init__(self, nolinks: int, tx: Transmitter, link: Link, bandwith: float = 5e9, os=5):
+        """
+        :param nolinks: Number of links in the vector of links.
+        """
+        self.transmitter = tx
+        self.bandwidth = bandwith
+        self.os = os
+
+        self.max_power = 10
+        self.average_power = 5
+        self.transmitter.mode = '6gtandem'
+        self.transmitter.amplifier.max_output_amplitude = self.max_power
+        self.transmitter.amplifier.set_average_power(15, self.average_power)
+
+        self.links = []
+        for l in range(nolinks):
+            link = Link()
+            link.amp.mode = '6gtandem'
+            link.amp.max_output_amplitude = self.max_power
+            link.amp.set_average_power(self.average_power - link.fiber.damping -
+                                       link.coupler_in.damping - link.coupler_out.damping, self.average_power)
+            link.amp.set_noise_var(300, self.bandwidth * self.os, 10)
+
+            self.links.append(link)
+
+        super().__init__()
+
+    def run(self, x):
+        y = np.zeros(np.shape(x), 1 + len(self.links))
+        y[:, 1] = self.transmitter.run(x)
+
+        for i, link in enumerate(self.links):
+            y[:, i+1] = link.run(y[:, i])
+
+    def calibrate(self, x, desired_amplifier_dbm):
+        """This function sets the small-signal gain of the link amplifiers.
+
+        To give an approximate constant power(DesiredAmplifierDBM) at the output of each link.
+        The calibration is valid for a given input signal, and recalibration must be performed if
+        the signal statistics changes.
+        """
+        for j in range(3):
+            z = self.transmitter.run(x)
+            scale = db_to_magnitude(
+                desired_amplifier_dbm - models.utils.getdbm(z))
+            self.transmitter.amplifier.gain = self.transmitter.amplifier.gain * scale
+
+        z = self.transmitter.run(x)
+        for link in self.links:
+            for j in range(3):
+                z2 = link.run(z)
+                scale = db_to_magnitude(
+                    desired_amplifier_dbm - models.utils.getdbm(z2))
+                link.amplifier.gain = link.amplifier.gain * scale
+
+            z = link.run(z)
