250299d0296be9802d2661b336669731c7243645
Tijl Schepens <tijl.schepens@kuleuven.be>
2025-11-06 11:34:21 +0100

    Merging the two downlink examples. Renaming the files to have one uplink and one downlink example.

--- examples/basic_sim_DL.py
+++ examples/basic_sim_DL.py
@@ -1,7 +1,6 @@
 import os
 """
-This script simulates a basic downlink transmission in a sub-THz radio stripe system using OFDM waveforms,
-assuming only IDEAL hardware (no hardware impairments are modeled).
+This script simulates a basic downlink transmission in a sub-THz radio stripe system using OFDM waveforms.
 
 It loads configuration parameters from a YAML file, generates OFDM signals, models radio stripes and their transmission,
 applies a wireless channel, and evaluates the received signal at a user equipment (UE) location.
@@ -25,10 +24,12 @@ Dependencies:
 
 Note:
 - The script is intended to be run as a standalone module.
-- Only IDEAL hardware is simulated (no hardware impairments).
+- This can be used to debug the ideal scenario without any hardware impairments or effects from the wireless channel.
 - Some configuration loading and equalization steps are marked as TODO.
 """
 import sys
+import logging
+
 # Add project root to sys.path for local imports
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 ###########################################
@@ -44,11 +45,20 @@ from sub_THz_stripe.central_unit.central_unit import CentralUnit
 from sub_THz_stripe.radio_unit.radio_unit import RadioUnit
 from wireless_channel.subTHz_channel import Channel
 from wireless_channel.waveforms import Waveform
-from utils import logger, calculate_psd_per_symbol
+from utils import setup_logging
 from plotter import plotter
 
+booster_stages = ["fiber", "coupler", "amplifier", "coupler"]
+tx_stages = ["fiber", "coupler", "splitter", "shifter", "amplifier"]
 
 if __name__ == "__main__":
+    # Logfile name the same as the current script name.
+    script_name = __file__.split(".")[0]
+    logfile = f"{script_name}.log"
+
+    setup_logging(logfile)
+    logger = logging.getLogger("6GTandemBasicDL")
+
     # read config file
     config_file = "office_config.yml"
     dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -61,7 +71,7 @@ if __name__ == "__main__":
 
     # plot full room with all stripes and all possible ue locations
     plotter.plot_room(config)
-    logger.debug("%d stripes in the room", len(config['radio_stripes']))
+    logger.debug("%d stripes in the room", len(config["radio_stripes"]))
 
     # construct waveform class
     waveform_config = config["waveform_config"]
@@ -75,71 +85,78 @@ if __name__ == "__main__":
     ofdm_time = wf.ofdm_modulate()  # shape: nr_ofdm_symb x (fftsize + cp length)
     wf.plot_psd(ofdm_time, nperseg=wf.fft_size)
 
-
     # sanity check
-    tx_freq_oversampled = wf.ofdm_time_to_freq(ofdm_time)  # uses your method
+    tx_freq_oversampled = wf.ofdm_time_to_freq(ofdm_time)
     tx_subc = wf.extract_subcarriers(tx_freq_oversampled)  # shape (n_sym, n_carriers)
 
     # check pilot magnitudes for the first OFDM symbol
-    print("pilot indices:", wf.pilot_indices)
-    print("pilot values (TX) for first symbol:", tx_subc[0, wf.pilot_indices])
-    print("pilot magnitudes mean:", np.mean(np.abs(tx_subc[:, wf.pilot_indices])))
+    logger.debug("pilot indices:", wf.pilot_indices)
+    logger.debug("pilot values (TX) for first symbol:", tx_subc[0, wf.pilot_indices])
+    logger.debug("pilot magnitudes mean:", np.mean(np.abs(tx_subc[:, wf.pilot_indices])))
 
     # build all radio stripes
     stripes = []
+    component_config = config["component_config"]
     for stripe_cfg in config["radio_stripes"]:
-        stripes.append(RadioStripe.from_config_locations(stripe_cfg, wf))
+        stripes.append(RadioStripe.from_config_locations(stripe_cfg, component_config, wf))
 
     # continue with 3 stripes, separated by 1m
-    stripes = [stripes[5]]#, stripes[6]]  # stripes[5:11:2]
-    active_ru_idxes = [1]#, 2]  # , 4, 6
-
+    stripes = [stripes[5]]
+    active_ru_idxes = [4]
     plotter.plot_stripes(config, stripes)
     for stripe_idx, stripe in enumerate(stripes):
         logger.debug("stripe: %d: %s", stripe_idx, stripe)
 
     # load channels
     ue_pos = config["ue_positions"][0]
-    channel = Channel.from_sionna(ue_pos, debug=True) # debug=True enables a dummy channel of all ones
+    # Setting debug=True enables a dummy channel of all ones.
+    channel = Channel.from_sionna(ue_pos)  # , debug=True)
     logger.debug("%s", channel)
 
     cu = CentralUnit() # todo are these configs loadable?
-    print(f'CU: {cu}')
+    logger.debug(f"CU: {cu}")
     ofdm_time_after_cu = cu.run(ofdm_time) # shape: nr_ofdm_symbols x (fft_size + cp length)
-    logger.debug('shape of ofdm timee: %s', ofdm_time_after_cu.shape)
-    logger.debug('np alike: %s', np.allclose(ofdm_time, ofdm_time_after_cu))
+    logger.debug("shape of ofdm timee: %s", ofdm_time_after_cu.shape)
+    logger.debug("np alike: %s", np.allclose(ofdm_time, ofdm_time_after_cu))
 
-    logger.debug('transmitting over the stripe...')
+    logger.debug("transmitting over the stripe...")
     iq_at_last_rus = []
     for stripe_idx, stripe in enumerate(stripes):
-        logger.debug('stripe: %d - active ru %d', stripe_idx, active_ru_idxes[stripe_idx])
+        logger.debug("stripe: %d - active ru %d", stripe_idx, active_ru_idxes[stripe_idx])
         stripe.active_unit = active_ru_idxes[stripe_idx]
 
-        wf.plot_iq_time(ofdm_time_after_cu, title="Before reshape")
-
-        iq_data = ofdm_time_after_cu.reshape(1, -1) # flatten to (1 x nr_iq_symbols)
+        iq_data = ofdm_time_after_cu.reshape(1, -1)  # flatten to (1 x nr_iq_symbols)
 
         iq_data = iq_data.reshape(wf.n_ofdm_symbols, -1)  # flatten to (1 x nr_iq_symbols)
-        wf.plot_iq_time(iq_data, title="After reshape")
+        stripe.calibrate(iq_data, -30)
 
-        logger.debug('shape of iq data: %s', iq_data.shape) # 1 d array
+        logger.debug("shape of iq data: %s", iq_data.shape)  # 1 d array
         phase_shifts = [0, 0, 0, 0]
         iq_out, imdata = stripe.transmit(iq_data, phase_shifts)
-        logger.debug('iq out shape: %s', iq_out.shape)
+
+        fig, fig2 = plotter.plot_im_data_tx(imdata, wf)
+
+        fig.savefig(f"am_am_plot_stripe{stripe_idx}.pdf")
+        fig2.savefig(f"spec_plot_stripe{stripe_idx}.pdf")
+
         iq_out_reshaped = iq_out.reshape(nr_antennas, wf.n_ofdm_symbols, -1)
-        logger.debug('reshaped after stripe: %s', iq_out_reshaped.shape)
+        logger.debug("reshaped after stripe: %s", iq_out_reshaped.shape)
         iq_at_last_rus.append(iq_out_reshaped)
 
+    iq_at_last_rus = np.array(iq_at_last_rus)
+    wf.plot_psd(iq_at_last_rus)
+
     y_ue = channel.transmit_dl(iq_at_last_rus, active_ru_idxes, wf)
-    logger.debug('received signal at ue: %s', y_ue.shape)
+    logger.debug(f"received signal at ue: {y_ue.shape}")
 
     wf.plot_iq_time(y_ue[0], title="After wireless channel")
+    wf.plot_psd(y_ue[0])
 
-    ue = RadioUnit(ue_pos['x'], ue_pos['y'], ue_pos['z'])
-    logger.debug('ue RU: %s', ue)
+    ue = RadioUnit(ue_pos["x"], ue_pos["y"], ue_pos["z"], wf)
+    logger.debug(f"ue RU: {ue}")
     shifts = [0, 0, 0, 0]
     y_combined_time, imdata = ue.receive(y_ue, shifts)
-    logger.debug('y combined shape: %s', y_combined_time.shape)
+    logger.debug(f"y combined shape: {y_combined_time.shape}")
 
     wf.plot_iq_time(y_combined_time, title="At UE")
 
@@ -149,8 +166,8 @@ if __name__ == "__main__":
     rx_freq_oversampled = wf.ofdm_time_to_freq(y_combined_time)  # your received time -> freq
     rx_subc = wf.extract_subcarriers(rx_freq_oversampled)
     # look at a few carriers around pilots and data
-    print("RX pilot bins first symbol:", rx_subc[0, wf.pilot_indices])
-    print("RX some data bins first symbol (first 10):", rx_subc[0, wf.data_carriers[:10]])
+    logger.debug(f"RX pilot bins first symbol: {rx_subc[0, wf.pilot_indices]}")
+    logger.debug(f"RX some data bins first symbol (first 10): {rx_subc[0, wf.data_carriers[:10]]}")
     wf.plot_constellation(rx_subc[0, wf.pilot_indices], title="received pilots")
 
     # channel estimation
@@ -158,19 +175,19 @@ if __name__ == "__main__":
     H_est = wf.channel_estimate_ls(subc)
 
     # sanity check
-    print("H_est shape:", H_est.shape)
+    logger.debug(f"H_est shape: {H_est.shape}")
     # show a summary for first symbol
-    print("H_est at pilot bins:", H_est[0, wf.pilot_indices])
-    print("H_est magnitude stats:", np.min(np.abs(H_est)), np.median(np.abs(H_est)), np.max(np.abs(H_est)))
+    logger.debug(f"H_est at pilot bins: {H_est[0, wf.pilot_indices]}")
+    logger.debug(f"H_est magnitude stats: {np.min(np.abs(H_est))}, {np.median(np.abs(H_est))}, {np.max(np.abs(H_est))}")
 
     # equalization
     eq_subc = wf.equalize_one_tap(subc, H_est)
 
     # sanity check
     i = wf.data_carriers[0]
-    print("raw rx on that carrier (first sym):", rx_subc[0, i])
-    print("H_est there:", H_est[0, i])
-    print("after equalize:", eq_subc[0, i])
+    logger.debug(f"raw rx on that carrier (first sym): {rx_subc[0, i]}")
+    logger.debug(f"H_est there: {H_est[0, i]}")
+    logger.debug(f"After equalize: {eq_subc[0, i]}")
 
     # Demap data carriers and rebuild stream
     data_symbols = wf.demap_data_from_grid(eq_subc).flatten()  # these are the received QAM symbols
@@ -185,5 +202,15 @@ if __name__ == "__main__":
     y_bits = wf.qam_to_bits(y_qam)
 
     ber = wf.compute_ber(bits, y_bits)
-    logger.debug('BER: %f', ber)
+    logger.debug(f"BER: {ber}")
     plt.show()
+
+    labels = [f"Symbol{n}" for n in range(y_combined_time.shape[0])]
+    fout = plotter.plot_iq_per_symbol(y_combined_time, wf.cp_length, wf.n_carriers, labels)
+    fout.savefig("iq_per_symbol.pdf")
+
+    fout = plotter.plot_iq_per_carrier(np.delete(y_combined_time, np.s_[1:], axis=0), wf.cp_length, wf.n_carriers)
+    fout.savefig("iq_per_carrier.pdf")
+
+    fout = plotter.plot_psd_per_symbol(y_combined_time, wf.fs)
+    fout.savefig("psd_per_symbol.pdf")
--- examples/basic_sim_UL.py
+++ examples/basic_sim_UL.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import logging
 
 # Add project root to sys.path for local imports
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -8,8 +7,20 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # DO NOT MOVE ANY IMPORTS ABOVE THIS LINE #
 ###########################################
 
+# todo add a RU to act as UE transmitter (splitter, phase shifters, PAs, disable coupler in!!)
+
+# todo transmit using RU
+
+# todo send over channel
+
+# todo test receive
+
+# todo test receive all
+
+from PySide6 import QtWidgets
 from matplotlib import pyplot as plt
 import numpy as np
+import logging
 import yaml
 
 from sub_THz_stripe.radiostripe.radiostripe import RadioStripe
@@ -17,13 +28,14 @@ from sub_THz_stripe.central_unit.central_unit import CentralUnit
 from sub_THz_stripe.radio_unit.radio_unit import RadioUnit
 from wireless_channel.subTHz_channel import Channel
 from wireless_channel.waveforms import Waveform
-from utils import calculate_psd_per_symbol, setup_logging
+from utils import setup_logging
 from plotter import plotter
 
 booster_stages = ["fiber", "coupler", "amplifier", "coupler"]
 tx_stages = ["fiber", "coupler", "splitter", "shifter", "amplifier"]
 
 if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
     # Logfile name the same as the current script name.
     script_name = __file__.split(".")[0]
     logfile = f"{script_name}.log"
@@ -41,90 +53,87 @@ if __name__ == "__main__":
     # todo load from config
     nr_antennas = 4
 
-    # plot full room with all stripes and all possible ue locations
-    plotter.plot_room(config)
-    logger.debug("%d stripes in the room", len(config["radio_stripes"]))
-
-    # construct waveform class
+    # Construct waveform class.
     waveform_config = config["waveform_config"]
     freq_band_config = config["sub_thz"]
     wf = Waveform.from_config(waveform_config, freq_band_config)
-    logger.debug("%s", wf)
+    logger.debug(f"{wf}")
 
-    # generate ofdm waveform in time domain
+    # Generate a CP-OFDM waveform in time domain.
     bits = wf.generate_bits()
     qam = wf.qam_modulate()
     ofdm_time = wf.ofdm_modulate()  # shape: nr_ofdm_symb x (fftsize + cp length)
     wf.plot_psd(ofdm_time)
 
-    # build all radio stripes
+    # Build all radio stripes.
     stripes = []
     component_config = config["component_config"]
     for stripe_cfg in config["radio_stripes"]:
         stripes.append(RadioStripe.from_config_locations(stripe_cfg, component_config, wf))
 
-    # continue with 3 stripes, separated by 1m
-    stripes = [stripes[5]]
-    active_ru_idxes = [4]  # , 4, 6
-    # , stripes[6]]  # stripes[5:11:2]
+    # As a simplified example only simulate the first two stripes.
+    # We cannot select stripes arbitrarily. Otherwise the stripe indexes no longer match the
+    # ones used by the channel. For example if we select only stripe 6 it gets index 0 in the
+    # stripes list. The channel will select the channel for stripe index 0 which does not
+    # match with the actual selected stripe. Same goes for selecting radio units.
+    stripes = stripes[0:2]
     plotter.plot_stripes(config, stripes)
     for stripe_idx, stripe in enumerate(stripes):
         logger.debug("stripe: %d: %s", stripe_idx, stripe)
 
-    # load channels
-    ue_pos = config["ue_positions"][0]
-    channel = Channel.from_sionna(ue_pos)  # , debug=True)
-    logger.debug("%s", channel)
+    # load channels. Select only a single UE position to start with. Same indexing remark
+    # applies here as for the stripes.
+    ue_pos = config["ue_positions"][3017]
+    channel = Channel.from_sionna(ue_pos, debug=True)
+    channel.Nr_stripes = 2
+    logger.debug(f"{channel}")
 
-    cu = CentralUnit()  # todo are these configs loadable?
+    # Currently we use the central unit at the UE side to transmit the OFDM signal.
+    cu = CentralUnit()  # TODO: are these configs loadable?
     logger.debug(f"CU: {cu}")
     ofdm_time_after_cu = cu.run(ofdm_time)  # shape: nr_ofdm_symbols x (fft_size + cp length)
-    logger.debug("shape of ofdm timee: %s", ofdm_time_after_cu.shape)
-    logger.debug("np alike: %s", np.allclose(ofdm_time, ofdm_time_after_cu))
+    logger.debug(f"shape of ofdm timee: {ofdm_time_after_cu.shape}")
+    logger.debug(f"np alike: {np.allclose(ofdm_time, ofdm_time_after_cu)}")
 
-    logger.debug("transmitting over the stripe...")
-    iq_at_last_rus = []
+    # We use a single radio unit to represent the UE.
+    ue = RadioUnit(ue_pos["x"], ue_pos["y"], ue_pos["z"], wf)
+    logger.debug(f"ue RU: {ue}")
+    shifts = [0, 0, 0, 0]
+    iq_data_tx, imdata = ue.transmit(ofdm_time_after_cu, shifts)
+    # Make an IQ-plot and PSD-plot of the data transmitted at the UE (one antenna).
+    wf.plot_iq_time(iq_data_tx[0], title="Transmitted at UE")
+    wf.plot_psd(iq_data_tx[0], title="Transmitted at UE")
+
+    iq_data_rus = channel.transmit_ul(iq_data_tx, wf)
+    wf.plot_psd(iq_data_rus[0][0][0], title="After wireless channel")
+
+    # Plot the received IQ-data for several RUs of the first stripe.
+    ru_ofdm_freqs = []
+    for i, ru_data in enumerate(iq_data_rus[0][0:10]):
+        ru_ofdm_freqs.append(wf.ofdm_time_to_freq(ru_data[0])[0])
+
+    labels = [f"RU{i+1}" for i in range(20)]
+    fig = plotter.plot_constellation(np.array(ru_ofdm_freqs), labels=labels, title="IQ received at the different RUs.")
+
+    logger.debug("Receiving data on all stripes.")
+    iq_stripes = []
     for stripe_idx, stripe in enumerate(stripes):
-        logger.debug("stripe: %d - active ru %d", stripe_idx, active_ru_idxes[stripe_idx])
-        stripe.active_unit = active_ru_idxes[stripe_idx]
-
-        iq_data = ofdm_time_after_cu.reshape(1, -1)  # flatten to (1 x nr_iq_symbols)
-
-        iq_data = iq_data.reshape(wf.n_ofdm_symbols, -1)  # flatten to (1 x nr_iq_symbols)
-        stripe.calibrate(iq_data, -30)
-
-        logger.debug("shape of iq data: %s", iq_data.shape)  # 1 d array
+        logger.debug(f"stripe: {stripe_idx}")
+        logger.debug("shape of iq data: %s", iq_data_rus.shape)
         phase_shifts = [0, 0, 0, 0]
-        iq_out, imdata = stripe.transmit(iq_data, phase_shifts)
+        iq_out, imdata = stripe.receive_all(iq_data_rus[stripe_idx], phase_shifts, window="dynamic")
 
-        fig, fig2 = plotter.plot_im_data_tx(imdata, wf)
+        fig, fig2 = plotter.plot_im_data_rx_all(imdata[:9*5], wf)
 
         fig.savefig(f"am_am_plot_stripe{stripe_idx}.pdf")
         fig2.savefig(f"spec_plot_stripe{stripe_idx}.pdf")
 
-        iq_out_reshaped = iq_out.reshape(nr_antennas, wf.n_ofdm_symbols, -1)
-        logger.debug("reshaped after stripe: %s", iq_out_reshaped.shape)
-        iq_at_last_rus.append(iq_out_reshaped)
+        iq_stripes.append(iq_out)
 
-    iq_at_last_rus = np.array(iq_at_last_rus)
-    wf.plot_psd(iq_at_last_rus)
-
-    y_ue = channel.transmit_dl(iq_at_last_rus, active_ru_idxes, wf)
-    logger.debug(f"received signal at ue: {y_ue.shape}")
-
-    wf.plot_iq_time(y_ue[0], title="After wireless channel")
-    wf.plot_psd(y_ue[0])
-
-    ue = RadioUnit(ue_pos["x"], ue_pos["y"], ue_pos["z"], wf)
-    logger.debug(f"ue RU: {ue}")
-    shifts = [0, 0, 0, 0]
-    y_combined_time, imdata = ue.receive(y_ue, shifts)
-    logger.debug(f"y combined shape: {y_combined_time.shape}")
-
-    y_combined_freq = wf.ofdm_time_to_freq(y_combined_time)
+    y_combined_freq = wf.ofdm_time_to_freq(iq_stripes[0])
 
     # sanity check
-    rx_freq_oversampled = wf.ofdm_time_to_freq(y_combined_time)  # your received time -> freq
+    rx_freq_oversampled = wf.ofdm_time_to_freq(iq_stripes[0])  # your received time -> freq
     rx_subc = wf.extract_subcarriers(rx_freq_oversampled)
     # look at a few carriers around pilots and data
     logger.debug(f"RX pilot bins first symbol: {rx_subc[0, wf.pilot_indices]}")
@@ -164,14 +173,9 @@ if __name__ == "__main__":
 
     ber = wf.compute_ber(bits, y_bits)
     logger.debug(f"BER: {ber}")
-    plt.show()
+    plt.show(block=True)
 
-    labels = [f"Symbol{n}" for n in range(y_combined_time.shape[0])]
-    fout = plotter.plot_iq_per_symbol(y_combined_time, wf.cp_length, wf.n_carriers, labels)
-    fout.savefig("iq_per_symbol.pdf")
+    widget = plotter.DynamicPlotter(imdata, len(stripes[0].radio_units), "rx", wf)
+    widget.show()
 
-    fout = plotter.plot_iq_per_carrier(np.delete(y_combined_time, np.s_[1:], axis=0), wf.cp_length, wf.n_carriers)
-    fout.savefig("iq_per_carrier.pdf")
-
-    fout = plotter.plot_psd_per_symbol(y_combined_time, wf.fs)
-    fout.savefig("psd_per_symbol.pdf")
+    sys.exit(app.exec())
