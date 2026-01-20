c5e9a0f721e1af2d0a7f7ea0ef64a84c0e29ade3
Tijl <tijl.schepens@kuleuven.be>
2026-01-20 16:56:05 +0100

    Incorporating polynomial amplifier model based on the practical measurements. Need to decide what to do with old polynomial models.

--- sub_THz_stripe/amplifier/amplifier.py
+++ sub_THz_stripe/amplifier/amplifier.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import pandas as pd
 import numpy.random
 
 from ..component.component import Component
@@ -24,7 +25,7 @@ class Amplifier(Component):
         >>> y = pa.run(x)
     """
 
-    def __init__(self, bw, gain=1, max_gain=1, max_out_amp=1, noise_fig=0, smoothness=1, *args, **kwargs):
+    def __init__(self, bw, gain=1, max_gain=1, noise_fig=0, smoothness=1, *args, **kwargs):
         """Initialize the amplifier object with the given parameters.
 
         :param gain: The low signal-gain of the amplifier.
@@ -33,15 +34,21 @@ class Amplifier(Component):
         :param noise_var: The variance of the AWGN [V^2] per channel.
         :param smoothness: Used in Mode 'softlimiter'.
         """
+        self.coeffs = None
+        self.max_input_amplitude = None
+        self.polynomial = None
         self._gain = 1
         self.max_gain = max_gain
         self.gain = gain
-        self.max_output_amplitude = max_out_amp
         self.noise_fig = noise_fig
         self.set_noise_var(25 + 273.15, bw, self.noise_fig)
         self.smoothness = smoothness
 
         super().__init__(*args, **kwargs)
+
+        if self.mode.startswith('poly'):
+            self.polynomial = int(self.mode[-1])
+            self.coeffs, self.max_input_amplitude = self.polynomial_fitting(self.polynomial, self.gain)
     
     @property
     def gain(self):
@@ -51,6 +58,10 @@ class Amplifier(Component):
     def gain(self, new_gain):
         assert new_gain <= self.max_gain, f"Gain is higher than the maximum achievable gain: {new_gain} > {self.max_gain}"
         self._gain = new_gain
+
+        # The polynomial coefficients have to be recalculated in case the gain changes.
+        if self.polynomial is not None:
+            self.coeffs, self.amax = self.polynomial_fitting(self.polynomial, self.gain)
 
     def set_maximum_output_power(self, max_power_dbm):
         """ Set the maximum output amplitude.
@@ -77,11 +88,32 @@ class Amplifier(Component):
         voltage_power = noise_power
         # Noise variance per channel.
         self.noise_var = voltage_power / 2
+    
+    def polynomial_fitting(self, order, gain):
+        # Read out the measurement data.
+        data = pd.read_csv('models/amplifier/Mag_150GHz_BalCascode2stage_V3.vcsv', skiprows=6, names=["x", "yre", "yim"])
+        # Go from the input values in dBm to values in Watt.
+        x_power = (10 ** (data["x"] / 10)) / 1000
+        # Now convert the values in Watt to Voltages.
+        x_voltage = np.sqrt(x_power * 50)
+        # Calculate the magnitude of the output voltage based on the real and complex values.
+        y_voltage = np.sqrt(data["yre"] ** 2 + data["yim"] ** 2)
+        # What is the current gain of the amplifier?
+        current_gain = ((y_voltage[4] - y_voltage[0]) / (x_voltage[4] - x_voltage[0]))
+        scale = current_gain / gain
+        # Re-scale the input and output to get the new gain.
+        x_scaled = x_voltage * scale
 
-    def run(self, x):
-        x = np.array(x)
-        x = self.gain / self.max_output_amplitude * (x + math.sqrt(self.noise_var) * (
-            numpy.random.normal(size=np.shape(x)) + 1j * numpy.random.normal(size=np.shape(x))))
+        # Only perform fitting for the odd powers.
+        odd_powers = list(range(1, order + 1, 2))
+        x_powers = np.column_stack([x_scaled ** p for p in odd_powers])
+    
+        coeffs, *_ = np.linalg.lstsq(x_powers, y_voltage, rcond=None)
+
+        return coeffs, np.max(x_scaled)
+
+    def run(self, x: np.ndarray):
+        x = (x + math.sqrt(self.noise_var) * (numpy.random.normal(size=np.shape(x)) + 1j * numpy.random.normal(size=np.shape(x))))
 
         match self.mode:
             case 'ideal' | 'linear':
@@ -113,12 +145,12 @@ class Amplifier(Component):
                 xout[abs(x) > peakx] = peakx * (1 - alpha * abs(peakx)
                                                 ** 2) * np.exp(1j * np.angle(xout[abs(x) > peakx]))
             case 'poly5':
-                # This factor gives 1dB compression at x=1
-                alpha = ((10 / 9) -
-                         np.sqrt(((20 / 9) * (10 ** -0.05)) - (80 / 81)))
-                # gives minimum derivative=0, at x=sqrt(2/3/alpha)
-                beta = (9 * (alpha ** 2)) / 20
-                xout = (x * (1 - (alpha * (abs(x) ** 2)) + (beta * (abs(x) ** 4))))
+                if self.polynomial is None or self.max_input_amplitude is None or self.coeffs is None:
+                    raise ValueError("Selected polynomial model but no coefficients configured")
+                xlim = x
+                xlim[abs(x) > self.max_input_amplitude] = self.max_input_amplitude
+                
+                xout = sum(c * x ** p for c, p in zip(self.coeffs, range(1, self.polynomial + 1, 2)))
             case 'limiter':
                 xout = x
                 # abs(xout) > 1 gives saturation at 1 V.
@@ -127,4 +159,4 @@ class Amplifier(Component):
             case 'softlimiter' | '6gtandem':
                 xout = softlimiter(x, self.smoothness)
 
-        return self.max_output_amplitude * xout
+        return xout
