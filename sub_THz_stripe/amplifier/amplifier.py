Fixing amplifier noise with an oversampling factor set above 1. Calculating noise variance is now correct. Noise levels in the example now match the CHA excel.

sub_THz_stripe/amplifier/amplifier.py
@@ -82,12 +82,10 @@ class Amplifier(Component):
 
     def set_noise_var(self, t, b, nfdb):
         k = 1.3806e-23
-        noise_density = k * t * db_to_power(nfdb)
-        noise_power = noise_density * b
-        # P = U^2/R
-        voltage_power = noise_power
-        # Noise variance per channel.
-        self.noise_var = voltage_power / 2
+        noise_power_watts =  10 ** (nfdb / 10) * k * t * b
+
+        self.noise_var = noise_power_watts
+
     
     def polynomial_fitting(self, order, gain):
         # Read out the measurement data.
@@ -109,16 +107,17 @@ class Amplifier(Component):
         x_powers = np.column_stack([x_scaled ** p for p in odd_powers])
     
         coeffs, *_ = np.linalg.lstsq(x_powers, y_voltage, rcond=None)
-        print(coeffs)
 
         return coeffs, np.max(x_scaled)
 
     def run(self, x: np.ndarray):
-        x = (x + math.sqrt(self.noise_var) * (numpy.random.normal(size=np.shape(x)) + 1j * numpy.random.normal(size=np.shape(x))))
+        noise = np.random.normal(0, np.sqrt(self.noise_var / 2), size=np.shape(x)) + 1j * numpy.random.normal(0, np.sqrt(self.noise_var / 2), size=np.shape(x))
+        print(f"Noise variance: {10 * np.log10(np.var(noise) * 1000)} dBm")
+        x_noise = x + noise
 
         match self.mode:
             case 'ideal' | 'linear':
-                xout = x * self.gain
+                xout = x_noise * self.gain
             case 'atan':
                 # alpha=0.6340 # This factor gives 1dB compression at x=1
                 alpha = 2 / np.pi  # This factor gives Amax=1;
@@ -148,8 +147,8 @@ class Amplifier(Component):
             case 'poly5':
                 if self.polynomial is None or self.max_input_amplitude is None or self.coeffs is None:
                     raise ValueError("Selected polynomial model but no coefficients configured")
-                xlim = x.copy()
-                xlim[np.abs(x) > self.max_input_amplitude] = self.max_input_amplitude
+                xlim = x_noise.copy()
+                xlim[np.abs(x_noise) > self.max_input_amplitude] = self.max_input_amplitude
                 
                 xout = sum(c * xlim * np.abs(xlim) ** (p-1) for c, p in zip(self.coeffs, range(1, self.polynomial + 1, 2)))
             case 'limiter':

sub_THz_stripe/radiostripe/radiostripe.py
@@ -13,7 +13,7 @@ from ..splitter.splitter import Splitter
 from ..combiner.combiner import Combiner
 from ..phase_shifter.phase_shifter import PhaseShifter
 from ..component.component import Component
-from ..utils import db_to_magnitude, getdbm
+from ..utils import db_to_magnitude, getdbm, calculate_nmse 
 
 logger = logging.getLogger(__name__)
 
@@ -106,6 +106,7 @@ class RadioStripe(Component):
         for i, (ru, fib) in enumerate(
             zip(self.radio_units[: self.active_unit + 1], self.fibers[: self.active_unit + 1])
         ):
+            sig_in = y.copy()
             y = fib.run(y)
             imdata.append(y)
             if i == self.active_unit:
@@ -114,6 +115,11 @@ class RadioStripe(Component):
             else:
                 y, imd = ru.boost(y)
                 imdata.extend(imd)
+                nmse, pnoise, psig = calculate_nmse(imdata[0], y, self.wf)
+                print(f"Psig_in stage{i}: {10 * np.log10(np.mean(np.abs(sig_in ** 2) * 1000))} dBm")
+                print(f"Psig stage{i}: {psig} dBm")
+                print(f"Pnoise stage{i}: {pnoise} dBm")
+                print(f"NMSE stage{i}: {nmse} dB")
 
         return y, imdata
 
@@ -278,8 +284,8 @@ class RadioStripe(Component):
             if "radio_unit" in unit_cfg:
                 coup_in = Coupler(wf)
                 coup_out = Coupler(wf)
-                boost_amp = Amplifier(**component_config.get("boost_amplifier", {}), bw=wf.bw)
-                antenna_amp = Amplifier(**component_config.get("antenna_amplifier", {}), bw=wf.bw)
+                boost_amp = Amplifier(**component_config.get("boost_amplifier", {}), bw=wf.bw*wf.oversampling_factor)
+                antenna_amp = Amplifier(**component_config.get("antenna_amplifier", {}), bw=wf.bw*wf.oversampling_factor)
 
                 if coupler_config is not None:
                     coup_in = Coupler.from_config(coupler_config, wf)

sub_THz_stripe/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import numpy.random
 
 from scipy.signal import lfilter
+import matplotlib.pyplot as plt
 from wireless_channel.waveforms import Waveform
 
 global_seed = None
@@ -57,11 +58,42 @@ def calculate_papr(data, percentile: float = 99.5):
     """
     abs_data = np.abs(data)
     threshold = np.percentile(abs_data, percentile)
-    sdata = abs_data[abs_data > threshold]
+    sdata = abs_data[abs_data < threshold]
 
     papr = np.max(sdata ** 2) / np.mean(sdata ** 2)
 
     return 10 * np.log10(papr)
+
+def calculate_nmse(sig_in: np.ndarray, sig_out: np.ndarray, wf: Waveform):
+    x_combined_freq = wf.ofdm_time_to_freq(sig_in)
+    xsubc = wf.extract_subcarriers(x_combined_freq)
+
+    # channel estimation
+    y_combined_freq = wf.ofdm_time_to_freq(sig_out)
+    ysubc = wf.extract_subcarriers(y_combined_freq)
+    H_est = wf.channel_estimate_ls(ysubc)
+
+    # equalization
+    eq_subc = wf.equalize_one_tap(ysubc, H_est)
+
+    plt.scatter(xsubc.real, xsubc.imag, label="tx")
+    plt.scatter(eq_subc.real, eq_subc.imag, label="rx")
+    plt.legend()
+    plt.show()
+    nmse = np.sum(np.abs(eq_subc - xsubc) ** 2) / np.sum(np.abs(xsubc) ** 2)
+
+    xsubc = wf.pad_subcarriers(xsubc)
+    x_time = wf.ofdm_freq_to_time(xsubc)
+    eq_subc = wf.pad_subcarriers(eq_subc)
+    y_time = wf.ofdm_freq_to_time(eq_subc)
+    print(f"Psig_in utils: {10 * np.log10(np.mean(np.abs(sig_in ** 2) * 1000))} dBm")
+    print(f"X_time utils: {10 * np.log10(np.mean(np.abs(x_time ** 2) * 1000))} dBm")
+
+    noise = y_time - x_time
+    noise = np.mean(np.abs(noise) ** 2)
+    x_time = np.mean(np.abs(x_time) ** 2)
+
+    return 10 * np.log10(nmse), 10 * np.log10(noise * 1000), 10 * np.log10(x_time * 1000)
 
 
 def getdbm(x):